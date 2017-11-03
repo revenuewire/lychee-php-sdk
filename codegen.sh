d87305ca7ff1c1a5ce09ed248bd68211884f8bca
Scott Wang <scott@malloc.sh>
2017-11-03 11:21:42 -0700

    init commit

---
+++ codegen.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir -p spec
+mkdir -p dist
+aws apigateway get-export --region us-west-2 --rest-api-id oee2l6t575 --stage-name v1 --export-type swagger spec/swagger.json
+swagger-codegen generate -i ./spec/swagger.json -c codegen-php-config.json -l php
